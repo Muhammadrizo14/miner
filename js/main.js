a976e62783f73d52b92233ce4dd9099f578553c2
Muhammadrizo <mtursunboev13@gmail.com>
2023-08-10 00:36:55 +0500

    addpopup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-document.querySelector('.copy__icon').addEventListener('click', ()=> {
+document.querySelector('.copy__icon').addEventListener('click', () => {
   navigator.clipboard.writeText('https://etheriums.online/?bonus=24655');
+})
+
+
+window.onload = () => {
+  document.querySelector('#preloader').style.display = 'none'
+}
+
+
+let profileImage = document.querySelector('.profile__image'),
+auth = document.querySelector('.auth'),
+authContent = document.querySelector('.auth__content')
+
+auth.addEventListener('click',()=> {
+  auth.style.display = 'none'
+  authContent.style.display = 'none'
+})
+
+
+profileImage.addEventListener('click', ()=> {
+  auth.style.display = 'block'
+  authContent.style.display = 'flex'
 })
